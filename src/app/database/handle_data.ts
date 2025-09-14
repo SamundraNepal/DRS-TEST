d71c4e1b4ff9b4e5b0392ff814e6c93b58f622ec
Samundra Nepal <samundranepal9818@gmail.com>
2025-09-14 19:31:29 +1000

    for testing purpose EC2

--- src/app/database/handle_data.ts
+++ src/app/database/handle_data.ts
@@ -1,8 +1,8 @@
-const api = 'http://localhost:3000/';
+const API_URL = process.env.NEXT_PUBLIC_API_URL; // e.g., http://52.62.243.131:3000
 
 export async function HandleSignUp({ formData }: any) {
   try {
-    const res = await fetch('/admin/api/sign_up', {
+    const res = await fetch(`${API_URL}/admin/api/sign_up`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData), // <-- body outside headers
@@ -22,7 +22,7 @@ export async function HandleSignUp({ formData }: any) {
 
 export async function HandleLogIn({ formData }: any) {
   try {
-    const res = await fetch('/admin/api/log_in', {
+    const res = await fetch(`${API_URL}/admin/api/log_in`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData), // <-- body outside headers
@@ -42,7 +42,7 @@ export async function HandleLogIn({ formData }: any) {
 
 export async function HandleAddDoctors({ formData }: any) {
   try {
-    const res = await fetch('/admin/api/admin_doctors', {
+    const res = await fetch(`${API_URL}/admin/api/admin_doctors`, {
       method: 'POST',
       body: formData, // <-- body outside headers
     });
@@ -61,7 +61,7 @@ export async function HandleAddDoctors({ formData }: any) {
 
 export async function HandleAddService({ formData }: any) {
   try {
-    const res = await fetch('/admin/api/admin_services', {
+    const res = await fetch(`${API_URL}/admin/api/admin_services`, {
       method: 'POST',
       body: formData, // <-- body outside headers
     });
@@ -80,7 +80,7 @@ export async function HandleAddService({ formData }: any) {
 
 export async function GetDoctorsData() {
   try {
-    const res = await fetch(`${api}/admin/api/get_doctors`, {
+    const res = await fetch(`${API_URL}/admin/api/get_doctors`, {
       method: 'GET',
     });
 
@@ -98,7 +98,7 @@ export async function GetDoctorsData() {
 
 export async function GetServiceData() {
   try {
-    const res = await fetch(`${api}/admin/api/get_service`, {
+    const res = await fetch(`${API_URL}/admin/api/get_service`, {
       method: 'GET',
     });
 
@@ -116,7 +116,7 @@ export async function GetServiceData() {
 
 export async function DeleteDoctorData(el: any, type: string) {
   try {
-    const res = await fetch(`${api}/admin/api/admin_delete_doctors`, {
+    const res = await fetch(`${API_URL}/admin/api/admin_delete_doctors`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ el, type }),
@@ -136,7 +136,7 @@ export async function DeleteDoctorData(el: any, type: string) {
 
 export async function AdminLogOut() {
   try {
-    const res = await fetch(`${api}/admin/api/admin_log_out`, {
+    const res = await fetch(`${API_URL}/admin/api/admin_log_out`, {
       method: 'POST',
     });
 
@@ -154,7 +154,7 @@ export async function AdminLogOut() {
 
 export async function HandleBookForm({ bookForm }: any) {
   try {
-    const res = await fetch('/admin/api/book_form', {
+    const res = await fetch(`${API_URL}/admin/api/book_form`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(bookForm), // <-- body outside headers
